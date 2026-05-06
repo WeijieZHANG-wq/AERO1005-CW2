88b9d3d3a009fc3636216d9b5339f0107908acc4
WeijieZHANG-wq <ssywz9@nottingham.edu.cn>
2026-05-06 12:26:42 +0800

    update

--- Weijie_ZHANG_20721142.m
+++ Weijie_ZHANG_20721142.m
@@ -22,18 +22,19 @@ duration = 600; % s
 voltages = zeros(1,duration);
 times = 0:duration-1;
 V_0C = 0.5 % 500mV
-T_c = 0.01 % 10 mV/C
+T_C = 0.01 % 10 mV/C
 
 for t = 1:duration
     voltages(t) = readVoltage(a, 'A0');
     pause(1);
 end
-temp = (voltages - V_0C) / TC;
+temp = (voltages - V_0C) / T_C;
 
 T_min = min(temp);
 T_max = max(temp);
 T_avg = mean(temp);
 
+% Plot a temperature/time graph
 plot(times/60, temp);
 xlabel('Time (min)'); ylabel('Temperature (°C)');
 title('Task 1: Temperature Recording');
@@ -51,6 +52,19 @@ fprintf('Min temp %.2f C\n', T_min);
 fprintf('Average temp %.2f C\n', T_avg);
 fprintf('Data logging terminated\n');
 
+% Write the data into txt file
+fid = fopen('capsule_temperature.txt', 'w');
+fprintf(fid, 'Data logging initiated - %s\n', datestr(now, 'dd/mm/yyyy'));
+fprintf(fid, 'Location - Nottingham\n');
+for min = 0:10
+    idx = min*60 + 1;
+    fprintf(fid, 'Minute %d\tTemperature %.2f C\n', min, temp(idx));
+end
+fprintf(fid, 'Max temp %.2f C\n', T_max);
+fprintf(fid, 'Min temp %.2f C\n', T_min);
+fprintf(fid, 'Average temp %.2f C\n', T_avg);
+fprintf(fid, 'Data logging terminated\n');
+fclose(fid);
 
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 

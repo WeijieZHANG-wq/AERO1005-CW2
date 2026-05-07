4e2b56cb9c6bbedb1c707134a9f050e8ed7dee6c
WeijieZHANG-wq <ssywz9@nottingham.edu.cn>
2026-05-07 10:43:50 +0800

    update

--- temp_monitor.m
+++ temp_monitor.m
@@ -14,4 +14,39 @@ xlabel('Time (s)'); ylabel('Temperature (°C)');
 title('Real-time Temperature Monitor');
 xlim([0 60]); ylim([15 30]);
 grid on;
+% Store data
+timeData = [];
+tempData = [];
+startTime = tic;
 
+while true
+    % Read temperature
+    voltage = readVoltage(a, 'A0');
+    temp = (voltage - 0.5) / 0.01;
+    % Record data
+    currentTime = toc(startTime);
+    timeData = [timeData, currentTime];
+    tempData = [tempData, temp];
+    % Update graph
+    set(hLine, 'XData', timeData, 'YData', tempData);
+    drawnow
+
+    % LED control
+    if temp >= 18 && temp <= 24
+        writeDigitalPin(a, greenPin, 1);
+        writeDigitalPin(a, yellowPin, 0);
+        writeDigitalPin(a, redPin, 0);
+    elseif temp < 18
+        writeDigitalPin(a, greenPin, 0);
+        writeDigitalPin(a, yellowPin, 1); pause(0.5);
+        writeDigitalPin(a, yellowPin, 0); pause(0.5);
+        writeDigitalPin(a, redPin, 0);
+    else  % temp > 24
+        writeDigitalPin(a, greenPin, 0);
+        writeDigitalPin(a, yellowPin, 0);
+        writeDigitalPin(a, redPin, 1); pause(0.25);
+        writeDigitalPin(a, redPin, 0); pause(0.25);
+    end
+
+    pause(1)
+end
